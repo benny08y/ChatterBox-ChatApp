61e3f57b807fe64d6a012334c57369483bbf67b9
benny <benny08y@gmail.com>
2018-11-29 21:59:39 -0800

    Chat bubbles in msging

---
+++ app/src/main/java/tcss450/uw/edu/chatapp/chats/MessageListAdapter.java
@@ -0,0 +1,131 @@
+package tcss450.uw.edu.chatapp.chats;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import tcss450.uw.edu.chatapp.R;
+
+public class MessageListAdapter extends RecyclerView.Adapter {
+    private static final int VIEW_TYPE_MESSAGE_SENT = 1;
+    private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
+
+    private Context mContext;
+    private List<Message> mMessageList;
+    private String mCurrentUserEmail;
+
+    public MessageListAdapter(Context context, List<Message> messageList, String email) {
+        mContext = context;
+        mMessageList = messageList;
+        mCurrentUserEmail = email;
+    }
+
+    // Determines the appropriate ViewType according to the sender of the message.
+    @Override
+    public int getItemViewType(int position) {
+        Message message = (Message) mMessageList.get(position);
+
+//        for (int i = 0; i < mMessageList.size(); i++){
+            if (message.getEmail().equals(mCurrentUserEmail)) {
+                // If the current user is the sender of the message
+                return VIEW_TYPE_MESSAGE_SENT;
+            } else {
+                // If some other user sent the message
+                return VIEW_TYPE_MESSAGE_RECEIVED;
+            }
+//        }
+//        return 0;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == VIEW_TYPE_MESSAGE_SENT){
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_sent, parent, false);
+            return new SentMessageHolder(view);
+        } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_recieved, parent, false);
+            return new ReceivedMessageHolder(view);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Message message = (Message) mMessageList.get(position);
+
+        switch (holder.getItemViewType()) {
+            case VIEW_TYPE_MESSAGE_SENT:
+                ((SentMessageHolder) holder).bind(message);
+                break;
+            case VIEW_TYPE_MESSAGE_RECEIVED:
+                ((ReceivedMessageHolder) holder).bind(message);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessageList.size();
+    }
+
+    private class SentMessageHolder extends RecyclerView.ViewHolder {
+        TextView messageText, timeText;
+
+        SentMessageHolder(View itemView) {
+            super(itemView);
+
+            messageText = (TextView) itemView.findViewById(R.id.text_message_body);
+//            timeText = (TextView) itemView.findViewById(R.id.text_message_time);
+        }
+
+        void bind(Message message) {
+            messageText.setText(message.getMessage());
+
+            // Format the stored timestamp into a readable String using method.
+//            timeText.setText(Utils.formatDateTime(message.getCreatedAt()));
+        }
+    }
+
+    public void addNewMessage(Message message){
+        mMessageList.add(message);
+        Log.d("MesgAdapter", "In adapter"+mMessageList.get(0) + " "+mMessageList.get(1));
+        notifyDataSetChanged();
+    }
+
+    private class ReceivedMessageHolder extends RecyclerView.ViewHolder {
+        TextView messageText, timeText, nameText;
+        ImageView profileImage;
+
+        ReceivedMessageHolder(View itemView) {
+            super(itemView);
+
+            messageText = (TextView) itemView.findViewById(R.id.text_message_body);
+            nameText = (TextView) itemView.findViewById(R.id.text_message_name);
+
+//            timeText = (TextView) itemView.findViewById(R.id.text_message_time);
+//            profileImage = (ImageView) itemView.findViewById(R.id.image_message_profile);
+        }
+
+        void bind(Message message) {
+            messageText.setText(message.getMessage());
+            nameText.setText(message.getNickname());
+
+//            // Format the stored timestamp into a readable String using method.
+//            timeText.setText(Utils.formatDateTime(message.getCreatedAt()));
+//            // Insert the profile image from the URL into the ImageView.
+//            Utils.displayRoundImageFromUrl(mContext, message.getSender().getProfileUrl(), profileImage);
+        }
+    }
+}
