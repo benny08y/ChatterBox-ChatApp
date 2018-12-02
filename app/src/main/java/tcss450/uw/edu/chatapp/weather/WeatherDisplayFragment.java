f16b2cf4d3bc84c55ca28f3754069c04bbdff764
Enoch Chan <enoch.chan2010@gmail.com>
2018-12-02 14:39:27 -0800

    saved locations list collapses

--- app/src/main/java/tcss450/uw/edu/chatapp/weather/WeatherDisplayFragment.java
+++ app/src/main/java/tcss450/uw/edu/chatapp/weather/WeatherDisplayFragment.java
@@ -1,4 +1,4 @@
-package tcss450.uw.edu.chatapp;
+package tcss450.uw.edu.chatapp.weather;
 
 
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import tcss450.uw.edu.chatapp.R;
 
 
 /**
